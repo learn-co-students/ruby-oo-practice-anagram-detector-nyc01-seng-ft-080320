3ce7bf42ce172c4e35757101cf5dfc6ba47ea050
anickel101 <anson.nickel@gmail.com>
2020-08-06 22:30:41 -0400

    Completed Lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+
+    attr_accessor :string
+
+    def initialize(string)
+        @string = string
+    end
+
+
+    def match(anagrams)
+        letters = self.string.chars.sort
+        anagrams.find_all {|anagram| anagram.chars.sort == letters}
+    end
+
+end
